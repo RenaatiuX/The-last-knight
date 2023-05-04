814f5fda7ad503d8dd16aa8d716cc9e5a8a0e4fd
Bretzelfresser <81638199+Bretzelfresser@users.noreply.github.com>
2023-05-04 22:55:49 +0200

    attack animations set

--- src/main/java/com/ren/lastknight/common/entity/Knight.java
+++ src/main/java/com/ren/lastknight/common/entity/Knight.java
@@ -2,10 +2,7 @@ package com.ren.lastknight.common.entity;
 
 import com.ren.lastknight.core.init.EntityInit;
 import net.minecraft.enchantment.EnchantmentHelper;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.MobEntity;
+import net.minecraft.entity.*;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.ai.attributes.AttributeModifierMap;
 import net.minecraft.entity.ai.attributes.Attributes;
@@ -23,9 +20,12 @@ import net.minecraft.nbt.ListNBT;
 import net.minecraft.network.datasync.DataParameter;
 import net.minecraft.network.datasync.DataSerializers;
 import net.minecraft.network.datasync.EntityDataManager;
+import net.minecraft.pathfinding.Path;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.EntityPredicates;
+import net.minecraft.util.Hand;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.RayTraceContext;
 import net.minecraft.util.math.RayTraceResult;
@@ -34,6 +34,7 @@ import net.minecraft.util.text.ITextComponent;
 import net.minecraft.world.BossInfo;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerBossInfo;
+import org.apache.http.conn.scheme.SchemeLayeredSocketFactory;
 import software.bernie.geckolib3.core.AnimationState;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.IAnimationTickable;
@@ -55,6 +56,7 @@ public class Knight extends MonsterEntity implements IAnimatable, IAnimationTick
 
     public static final DataParameter<Integer> CHARGING = EntityDataManager.createKey(Knight.class, DataSerializers.VARINT);
     public static final DataParameter<Boolean> SUMMONING = EntityDataManager.createKey(Knight.class, DataSerializers.BOOLEAN);
+    public static final DataParameter<Boolean> BIG_ATTACK = EntityDataManager.createKey(Knight.class, DataSerializers.BOOLEAN);
 
     public static final int MAX_CHARGING = 50;
 
@@ -70,7 +72,7 @@ public class Knight extends MonsterEntity implements IAnimatable, IAnimationTick
     protected void registerGoals() {
         this.goalSelector.addGoal(0, new SwimGoal(this));
         this.goalSelector.addGoal(0, new SummonMinionsGoal(this));
-        this.goalSelector.addGoal(1, new MeleeAttackGoal(this, 1.0D, true));
+        this.goalSelector.addGoal(1, new KnightMeleeAttackGoal(this, 1.0D, true, 0.1f));
         this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, PlayerEntity.class, true));
         this.goalSelector.addGoal(2, new WaterAvoidingRandomWalkingGoal(this, 0.8D));
         this.goalSelector.addGoal(3, new LookAtGoal(this, PlayerEntity.class, 8.0F));
@@ -92,6 +94,7 @@ public class Knight extends MonsterEntity implements IAnimatable, IAnimationTick
         super.registerData();
         this.dataManager.register(CHARGING, 0);
         this.dataManager.register(SUMMONING, false);
+        this.dataManager.register(BIG_ATTACK, false);
     }
 
     @Override
@@ -102,6 +105,7 @@ public class Knight extends MonsterEntity implements IAnimatable, IAnimationTick
         }
         this.dataManager.set(CHARGING, nbt.getInt("charging"));
         this.dataManager.set(SUMMONING, nbt.getBoolean("summoning"));
+        this.amountMinions = nbt.getInt("amountMinions");
     }
 
 
@@ -110,6 +114,7 @@ public class Knight extends MonsterEntity implements IAnimatable, IAnimationTick
         super.writeAdditional(nbt);
         nbt.putInt("charging", this.dataManager.get(CHARGING));
         nbt.putBoolean("summoning", this.dataManager.get(SUMMONING));
+        nbt.putInt("amountMinions", this.amountMinions);
     }
 
 
@@ -175,6 +180,8 @@ public class Knight extends MonsterEntity implements IAnimatable, IAnimationTick
         littleKnight4.setKnight(this);
         littleKnight4.setAttackTarget(this.getAttackTarget());
         world.addEntity(littleKnight4);
+
+        setAmountMinions(4);
     }
 
     @Override
@@ -220,7 +227,11 @@ public class Knight extends MonsterEntity implements IAnimatable, IAnimationTick
     private PlayState attackPredicate(AnimationEvent<Knight> event) {
         if (this.isSwingInProgress && event.getController().getAnimationState().equals(AnimationState.Stopped)) {
             event.getController().markNeedsReload();
-            event.getController().setAnimation(new AnimationBuilder().addAnimation("Attack2.knight.new", ILoopType.EDefaultLoopTypes.PLAY_ONCE));
+            System.out.println(hasBigAttack());
+            if (hasBigAttack())
+                event.getController().setAnimation(new AnimationBuilder().addAnimation("Attack1.knight.new", ILoopType.EDefaultLoopTypes.PLAY_ONCE));
+            else
+                event.getController().setAnimation(new AnimationBuilder().addAnimation("Attack2.knight.new", ILoopType.EDefaultLoopTypes.PLAY_ONCE));
             this.isSwingInProgress = false;
         }
         return PlayState.CONTINUE;
@@ -228,6 +239,7 @@ public class Knight extends MonsterEntity implements IAnimatable, IAnimationTick
 
     @Override
     protected void damageEntity(DamageSource damageSrc, float damageAmount) {
+        damageAmount *= 1.2f;
         super.damageEntity(damageSrc, damageAmount);
         addCharging((int) damageAmount);
     }
@@ -267,6 +279,14 @@ public class Knight extends MonsterEntity implements IAnimatable, IAnimationTick
         return flag;
     }
 
+    private void setBigAttack(boolean bigAttack) {
+        this.dataManager.set(BIG_ATTACK, bigAttack);
+    }
+
+    private boolean hasBigAttack() {
+        return this.dataManager.get(BIG_ATTACK);
+    }
+
     protected int getCharging() {
         return this.dataManager.get(CHARGING);
     }
@@ -280,7 +300,7 @@ public class Knight extends MonsterEntity implements IAnimatable, IAnimationTick
     }
 
     void setAmountMinions(int amountMinions) {
-        this.amountMinions = Math.min(0, amountMinions);
+        this.amountMinions = Math.max(0, amountMinions);
     }
 
     void reduceAmountMinions() {
@@ -318,6 +338,179 @@ public class Knight extends MonsterEntity implements IAnimatable, IAnimationTick
             }
         }
 
+    }
+
+    private static class KnightMeleeAttackGoal extends Goal {
+
+        protected final Knight attacker;
+        private final double speedTowardsTarget;
+        private final boolean longMemory;
+        private Path path;
+        private double targetX;
+        private double targetY;
+        private double targetZ;
+        private int delayCounter;
+        private int swingCooldown;
+        private final int attackInterval = 20;
+        private long lastCheckTime;
+        private int failedPathFindingPenalty = 0;
+        private boolean canPenalize = false;
+        private final float chanceBigAttack;
+
+        public KnightMeleeAttackGoal(Knight creature, double speedIn, boolean useLongMemory, float chanceBigAttack) {
+            this.attacker = creature;
+            this.speedTowardsTarget = speedIn;
+            this.longMemory = useLongMemory;
+            this.chanceBigAttack = chanceBigAttack;
+            this.setMutexFlags(EnumSet.of(Goal.Flag.MOVE, Goal.Flag.LOOK));
+        }
+
+        /**
+         * Returns whether execution should begin. You can also read and cache any state necessary for execution in this
+         * method as well.
+         */
+        public boolean shouldExecute() {
+            long i = this.attacker.world.getGameTime();
+            if (i - this.lastCheckTime < 20L) {
+                return false;
+            } else {
+                this.lastCheckTime = i;
+                LivingEntity livingentity = this.attacker.getAttackTarget();
+                if (livingentity == null) {
+                    return false;
+                } else if (!livingentity.isAlive()) {
+                    return false;
+                } else {
+                    if (canPenalize) {
+                        if (--this.delayCounter <= 0) {
+                            this.path = this.attacker.getNavigator().pathfind(livingentity, 0);
+                            this.delayCounter = 4 + this.attacker.getRNG().nextInt(7);
+                            return this.path != null;
+                        } else {
+                            return true;
+                        }
+                    }
+                    this.path = this.attacker.getNavigator().pathfind(livingentity, 0);
+                    if (this.path != null) {
+                        return true;
+                    } else {
+                        return this.getAttackReachSqr(livingentity) >= this.attacker.getDistanceSq(livingentity.getPosX(), livingentity.getPosY(), livingentity.getPosZ());
+                    }
+                }
+            }
+        }
+
+        /**
+         * Returns whether an in-progress EntityAIBase should continue executing
+         */
+        public boolean shouldContinueExecuting() {
+            LivingEntity livingentity = this.attacker.getAttackTarget();
+            if (livingentity == null) {
+                return false;
+            } else if (!livingentity.isAlive()) {
+                return false;
+            } else if (!this.longMemory) {
+                return !this.attacker.getNavigator().noPath();
+            } else if (!this.attacker.isWithinHomeDistanceFromPosition(livingentity.getPosition())) {
+                return false;
+            } else {
+                return !(livingentity instanceof PlayerEntity) || !livingentity.isSpectator() && !((PlayerEntity) livingentity).isCreative();
+            }
+        }
+
+        /**
+         * Execute a one shot task or start executing a continuous task
+         */
+        public void startExecuting() {
+            this.attacker.getNavigator().setPath(this.path, this.speedTowardsTarget);
+            this.attacker.setAggroed(true);
+            this.delayCounter = 0;
+            this.swingCooldown = 0;
+        }
+
+        /**
+         * Reset the task's internal state. Called when this task is interrupted by another one
+         */
+        public void resetTask() {
+            LivingEntity livingentity = this.attacker.getAttackTarget();
+            if (!EntityPredicates.CAN_AI_TARGET.test(livingentity)) {
+                this.attacker.setAttackTarget((LivingEntity) null);
+            }
+
+            this.attacker.setAggroed(false);
+            this.attacker.getNavigator().clearPath();
+        }
+
+        /**
+         * Keep ticking a continuous task that has already been started
+         */
+        public void tick() {
+            LivingEntity livingentity = this.attacker.getAttackTarget();
+            this.attacker.getLookController().setLookPositionWithEntity(livingentity, 30.0F, 30.0F);
+            double d0 = this.attacker.getDistanceSq(livingentity.getPosX(), livingentity.getPosY(), livingentity.getPosZ());
+            this.delayCounter = Math.max(this.delayCounter - 1, 0);
+            if ((this.longMemory || this.attacker.getEntitySenses().canSee(livingentity)) && this.delayCounter <= 0 && (this.targetX == 0.0D && this.targetY == 0.0D && this.targetZ == 0.0D || livingentity.getDistanceSq(this.targetX, this.targetY, this.targetZ) >= 1.0D || this.attacker.getRNG().nextFloat() < 0.05F)) {
+                this.targetX = livingentity.getPosX();
+                this.targetY = livingentity.getPosY();
+                this.targetZ = livingentity.getPosZ();
+                this.delayCounter = 4 + this.attacker.getRNG().nextInt(7);
+                if (this.canPenalize) {
+                    this.delayCounter += failedPathFindingPenalty;
+                    if (this.attacker.getNavigator().getPath() != null) {
+                        net.minecraft.pathfinding.PathPoint finalPathPoint = this.attacker.getNavigator().getPath().getFinalPathPoint();
+                        if (finalPathPoint != null && livingentity.getDistanceSq(finalPathPoint.x, finalPathPoint.y, finalPathPoint.z) < 1)
+                            failedPathFindingPenalty = 0;
+                        else
+                            failedPathFindingPenalty += 10;
+                    } else {
+                        failedPathFindingPenalty += 10;
+                    }
+                }
+                if (d0 > 1024.0D) {
+                    this.delayCounter += 10;
+                } else if (d0 > 256.0D) {
+                    this.delayCounter += 5;
+                }
+
+                if (!this.attacker.getNavigator().tryMoveToEntityLiving(livingentity, this.speedTowardsTarget)) {
+                    this.delayCounter += 15;
+                }
+            }
+
+            this.swingCooldown = Math.max(this.swingCooldown - 1, 0);
+            this.checkAndPerformAttack(livingentity, d0);
+        }
+
+        protected void checkAndPerformAttack(LivingEntity enemy, double distToEnemySqr) {
+            double d0 = this.getAttackReachSqr(enemy);
+            if (distToEnemySqr <= d0 && this.swingCooldown <= 0) {
+                this.resetSwingCooldown();
+                this.attacker.setBigAttack(this.attacker.getRNG().nextFloat() <= chanceBigAttack);
+                this.attacker.swingArm(Hand.MAIN_HAND);
+                this.attacker.attackEntityAsMob(enemy);
+            }
+
+        }
+
+        protected void resetSwingCooldown() {
+            this.swingCooldown = attackInterval;
+        }
+
+        protected boolean isSwingOnCooldown() {
+            return this.swingCooldown <= 0;
+        }
+
+        protected int getSwingCooldown() {
+            return this.swingCooldown;
+        }
+
+        protected int func_234042_k_() {
+            return 20;
+        }
+
+        protected double getAttackReachSqr(LivingEntity attackTarget) {
+            return (double) (this.attacker.getWidth() * 2.0F * this.attacker.getWidth() * 2.0F + attackTarget.getWidth());
+        }
     }
 
 }
